92ad41f2f80f6523a88baea5a613e7ebc9c1cdb2
SeHyunJeon <uhs2921@gmail.com>
2025-06-09 23:40:54 +0900

    feat: Add new project 'stateful_widget'

---
+++ statefulWidget/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:stateful_widget/Screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
+
