971ad62624dfd1671edf370ec00e47911f0e4a5c
SeHyunJeon <uhs2921@gmail.com>
2025-06-24 00:57:00 +0900

    feat: implement a feature in 'u_and_i'

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
           width: MediaQuery.of(context).size.width, // 현재 실행 중인 앱의 사이즈
           child: Column(
             children: [
-              _Top(),
+              _Top(
+                selectedDate: selectedDate,
+                onPressed: onHeartPressed,
+              ),
+
+              /// 이미지
               _Bottom(),
             ],
           ),
@@ -26,38 +34,64 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+
+  void onHeartPressed(){
+    showCupertinoDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.center,
+          // 어디로 정렬될 지를 모르면 크기 등을 무시하고 화면의 전체를 차지
+          child: Container(
+            color: Colors.white,
+            height: 300.0,
+            child: CupertinoDatePicker(
+              maximumDate: DateTime.now(),
+              initialDateTime: selectedDate,
+              mode: CupertinoDatePickerMode.date,
+              dateOrder: DatePickerDateOrder.ymd,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  selectedDate = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
 class _Top extends StatelessWidget {
-  const _Top({super.key});
+  final DateTime selectedDate;
+  final VoidCallback? onPressed;
+
+  const _Top({required this.selectedDate, required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     final textTheme = Theme.of(context).textTheme;
     return Expanded(
       child: Container(
         child: Column(
           children: [
+            Text('U&I', style: textTheme.displayLarge),
+            Text('우리 처음 만난 날', style: textTheme.bodyLarge),
             Text(
-              'U&I',
-              style: textTheme.displayLarge,
-            ),
-            Text(
-              '우리 처음 만난 날',
-              style: textTheme.bodyLarge,
-            ),
-            Text(
-              '2025.02.09',
+              '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
               style: textTheme.bodyMedium,
             ),
             IconButton(
               iconSize: 60.0,
               color: Colors.red[400],
-              onPressed: () {},
+              onPressed: onPressed,
               icon: Icon(Icons.favorite),
             ),
             Text(
-              'D+1',
+              'D+${now.difference(selectedDate).inDays + 1}',
               style: textTheme.displayMedium,
             ),
           ],
@@ -72,11 +106,8 @@ class _Bottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Expanded(
-      child: Container(
-        child: Image.asset('asset/img/middle_image.png'),
-      ),
+    return Expanded(
+      child: Container(child: Image.asset('asset/img/middle_image.png')),
     );
   }
 }
-
