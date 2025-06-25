1d31dda90b45b938e78bfabbf35ae193beb897e0
SeHyunJeon <uhs2921@gmail.com>
2025-06-25 23:01:39 +0900

    fix: Add missing file in 'random_number_generator'

---
+++ random_number_generator/lib/screen/home_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> numbers = [123, 456, 789];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /// 제목과 아이콘 버튼이 있는 곳
+              _Header(),
+
+              /// 숫자가 있는 곳
+              _Body(numbers: numbers),
+
+              /// 버튼이 있는 곳
+              _Footer(
+                onPressed: () {
+                  setState(() {
+                    numbers = [999,888,777];
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.settings),
+          color: redColor,
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> numbers;
+
+  const _Body({required this.numbers, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: numbers
+            .map(
+              (e) => Row(
+                children: e
+                    .toString()
+                    .split('')
+                    .map(
+                      (number) => Image.asset(
+                        'asset/img/$number.png',
+                        width: 50.0,
+                        height: 70.0,
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Footer({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text('생성하기!'),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
